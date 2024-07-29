d9673ced2c1d1956e39d9ae8b5c6c6f535766d4d
Badnameless <francis.beltre.t@gmail.com>
2024-07-29 02:56:00 -0400

    Error fecha on null arreglado

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,27 +9,36 @@ use App\Models\Blog;
 class HomeController extends Controller
 {
     public function ShowIndex(){
-        $mainblog = Blog::orderBy('views', 'desc')->first();
-        $mainblog->fecha = Carbon::parse($mainblog->fecha)->format('F j, Y');
 
-        $popularblogs = Blog::orderBy('views', 'desc')->where('views', '<', $mainblog->views)->take(2)->get();
-        foreach ($popularblogs as $popularblog) {
-            $popularblog->fecha = Carbon::parse($popularblog->fecha)->format('F j, Y');
-        }
+        $blogs = Blog::all();
 
-        $dateblogs = Blog::orderBy('fecha', 'desc')->offset(3)->limit(PHP_INT_MAX)->take(3)->get();
-        foreach ($dateblogs as $dateblog) {
-            $dateblog->fecha = Carbon::parse($dateblog->fecha)->format('F j, Y');
-        }
+        if ($blogs) {
+            $mainblog = Blog::orderBy('views', 'desc')->first();
 
-        $mostpopularblogs = Blog::orderBy('views', 'desc')->get();
-        foreach ($mostpopularblogs as $key => $mostpopularblog) {
-            $mostpopularblog->fecha = Carbon::parse($mostpopularblog->fecha)->format('F j, Y');
-        }
+            if ($mainblog) {
+                $mainblog->fecha = Carbon::parse($mainblog->fecha)->format('F j, Y');
+            }
 
-        $recentblogs = Blog::orderBy('fecha', 'desc')->take(4)->get();
-        foreach ($recentblogs as $key => $recentblog) {
-            $recentblog->fecha = Carbon::parse($recentblog->fecha)->format('F j, Y');
+            $popularblogs = Blog::orderBy('views', 'desc')->where('views', '<', $mainblog->views)->take(2)->get();
+
+            foreach ($popularblogs as $popularblog) {
+                $popularblog->fecha = Carbon::parse($popularblog->fecha)->format('F j, Y');
+            }
+
+            $dateblogs = Blog::orderBy('fecha', 'desc')->offset(3)->limit(PHP_INT_MAX)->take(3)->get();
+            foreach ($dateblogs as $dateblog) {
+                $dateblog->fecha = Carbon::parse($dateblog->fecha)->format('F j, Y');
+            }
+
+            $mostpopularblogs = Blog::orderBy('views', 'desc')->get();
+            foreach ($mostpopularblogs as $key => $mostpopularblog) {
+                $mostpopularblog->fecha = Carbon::parse($mostpopularblog->fecha)->format('F j, Y');
+            }
+
+            $recentblogs = Blog::orderBy('fecha', 'desc')->take(4)->get();
+            foreach ($recentblogs as $key => $recentblog) {
+                $recentblog->fecha = Carbon::parse($recentblog->fecha)->format('F j, Y');
+            }
         }
 
         return view('index', compact('mainblog', 'popularblogs', 'dateblogs', 'mostpopularblogs', 'recentblogs'));
